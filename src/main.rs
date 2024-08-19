d2af31466a1a023f306d35892ed36f1c10b47055
SealJonny <jon.fri14@gmail.com>
2024-08-19 18:09:51 +0200

    first succes with upload_file

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,31 @@
 mod nextcloud;
 
 use nextcloud::NextcloudClient;
+use std::env;
+use std::path::Path;
 
 fn main() {
-    println!("Hello, world!");
-    let client = NextcloudClient::new("https://nextcloud.example.com");
-    let _re = client.upload_file("/home/test/Converted/Steps.jpg");
+    let path = Path::new(".env");
+    dotenv::from_path(path).expect("Failed to read .env file");
+
+    // Helper function to retrieve environment variables
+    fn get_env_var(var_name: &str) -> String {
+        env::var(var_name).unwrap_or_else(|e| {
+            eprintln!("Error while reading '{}': {}", var_name, e);
+            String::new()
+        })
+    }
+
+    let server_url = get_env_var("SERVER_URL");
+    let username = get_env_var("NC_USERNAME");
+    let password = get_env_var("PASSWORD");
+
+    println!("{}:{}@{}", username, password, server_url);
+
+    let client = NextcloudClient::new(&server_url, &username, &password);
+    
+    match client.upload_file("Vineyard.jpg") {
+        Ok(_val) => print!("Successfully uploaded demo!"),
+        Err(e) => eprint!("Error while uploading demo: {}", e)
+    }
 }
--- src/nextcloud.rs
+++ src/nextcloud.rs
@@ -1,31 +1,35 @@
 use reqwest::blocking::Client;
+use std::fmt::format;
 use std::fs::File;
 use std::io::Read;
 use std::error::Error;
-use std::path::Path;
 
 pub struct NextcloudClient {
     url_server: String,
+    username: String,
+    password: String,
     client: Client
 }
 
 impl NextcloudClient {
 
-    pub fn new(url_server: &str) -> NextcloudClient {
+    pub fn new(url_server: &str, username: &str, password: &str) -> NextcloudClient {
         return NextcloudClient{
-            url_server: url_server.to_string(),
+            url_server: url_server.to_string() + "/remote.php/dav/files/" + username,
+            username: username.to_string(),
+            password: password.to_string(),
             client: Client::new()
         }
     }
 
     pub fn upload_file(&self, local_path: &str) -> Result<(), Box<dyn Error>> {
-        let path = Path::new(local_path);
-        let mut file = File::open(path)?;
+        let mut file = File::open(local_path)?;
         let mut file_contents = Vec::new();
         file.read_to_end(&mut file_contents)?;
 
-        let response = self.client.put(self.url_server.as_str())
-            .basic_auth("admin", Some("admin"))
+        let url = format!("{}/{}", self.url_server, local_path);
+        let response = self.client.put(url.as_str())
+            .basic_auth(self.username.clone(), Some(self.password.clone()))
             .body(file_contents)
             .send()?;
 
